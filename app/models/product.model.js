5f0ea2081f49eb3955cc9d91f44ce696f317512e
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-14 15:51:35 +0700

    create model for product service

---
+++ app/models/product.model.js
@@ -0,0 +1,70 @@
+const db = require("../../config/dbConnection");
+
+const select = (callback) => {
+  const selectQuery = `SELECT p.id, p.name, c.name AS category, p.quantity, p.price, p.image_url FROM product AS p JOIN product_category AS pc ON p.id = pc.product_id JOIN category AS c ON pc.category_id = c.id WHERE is_deleted = FALSE ORDER BY p.id;`;
+  db.query(selectQuery, callback);
+};
+
+const selectByCategory = (params, callback) => {
+  const selectQuery = `SELECT p.id, p.name, c.name AS category, p.quantity, p.price, p.image_url FROM product AS p JOIN product_category AS pc ON p.id = pc.product_id JOIN category AS c ON pc.category_id = c.id WHERE is_deleted = FALSE AND c.name = ? ORDER BY p.id;`;
+  db.query(selectQuery, params, callback);
+};
+
+const selectProductId = (params, callback) => {
+  const selectQuery = `SELECT id FROM product WHERE name = ? AND price = ? AND image_url = ?;`;
+  db.query(selectQuery, params, callback);
+};
+
+const selectCategoryId = (params, callback) => {
+  const selectQuery = `SELECT id FROM category WHERE name = ?;`;
+  db.query(selectQuery, params, callback);
+};
+
+const create = (params, callback) => {
+  const insertQuery = `INSERT INTO product (name, quantity, price, image_url) VALUES (?, ?, ?, ?);`;
+  db.query(insertQuery, params, callback);
+};
+
+const createProductCategory = (params, callback) => {
+  const insertQuery = `INSERT INTO product_category (product_id, category_id) VALUES (?, ?);`;
+  db.query(insertQuery, params, callback);
+};
+
+const updateProductCategory = (params, callback) => {
+  const updateQuery = `UPDATE product_category SET category_id = ? WHERE category_id = ? AND product_id = ?;`;
+  db.query(updateQuery, params, callback);
+};
+
+const update = (params, callback) => {
+  const updateQuery = `UPDATE product SET name = ?, quantity = ?, price = ?, image_url = ?, updated_at = CURRENT_TIMESTAMP() WHERE id = ?;`;
+  db.query(updateQuery, params, callback);
+};
+
+const selectImageUrl = (params, callback) => {
+  const selectQuery = `SELECT image_url FROM product WHERE id = ?;`;
+  db.query(selectQuery, params, callback);
+};
+
+const selectProductById = (params, callback) => {
+  const selectQuery = `SELECT p.id, p.name, c.name AS category, p.quantity, p.price, p.image_url FROM product AS p JOIN product_category AS pc ON p.id = pc.product_id JOIN category AS c ON pc.category_id = c.id WHERE p.id = ?;`;
+  db.query(selectQuery, params, callback);
+};
+
+const softDelete = (params, callback) => {
+  const updateQuery = `UPDATE product SET is_deleted = TRUE, updated_at = CURRENT_TIMESTAMP() WHERE id = ?;`;
+  db.query(updateQuery, params, callback);
+};
+
+module.exports = {
+  select,
+  selectByCategory,
+  selectProductId,
+  selectCategoryId,
+  create,
+  createProductCategory,
+  updateProductCategory,
+  update,
+  selectImageUrl,
+  selectProductById,
+  softDelete,
+};

a4b7518203b2311cdd4f8af2de1af53b89443a61
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-19 17:19:52 +0700

    modify db config

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1pV7DJcm2W",
-  database: "yourgaminggear",
+  host: "YOUR_MYSQL_HOST", // "localhost" by default
+  user: "YOUR_MYSQL_USER", // "root" by default
+  password: "YOUR_MYSQL_PASSWORD",
+  database: "buanastore",
 });
 
 module.exports = db;
